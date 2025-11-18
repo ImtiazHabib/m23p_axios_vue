00ae634def614ab47041dd3a585b9080b42dd8a6
ImtiazHabib <imtiazhabib7@gmail.com>
2025-11-18 12:14:44 +0600

    Axios apply and api fetch

---
+++ src/components/postCards.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+// taking post from postpage
+const props = defineProps({
+    posts:{
+        type:Object,
+    } 
+});
+</script>
+
+<template >
+   
+   <template  v-for="post in posts">
+     <div class="col-md-4">
+         <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ post.title }}</h5>
+                        <p class="card-text">{{ post.body }}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+    </div>
+   </template>
+   
+              
+</template>
+
+<style >
+    
+</style>
---
+++ src/pages/postPage.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { onMounted,ref } from 'vue';
+import PostCards from '../components/postCards.vue';
+import Navbar from './parts/Navbar.vue';
+import axios from 'axios';
+ 
+
+// stroing data 
+const posts = ref({});
+
+// fetching data from json placholder api 
+onMounted( async() =>{
+    const data = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+    posts.value = data.data;
+}
+
+)
+
+</script>
+
+<template>
+   <Navbar />
+
+    <!-- main section -->
+     <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>
+                        Hello Welcome to axios vue
+                    </h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- sending post data to the vue file  -->
+                   <PostCards  :posts="posts" />
+            </div>
+        </div>
+     </section>
+</template>
+
+<style></style>
